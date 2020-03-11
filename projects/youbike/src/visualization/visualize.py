8ab7db6d81e6818f5d0d89e1a29efe255cdc8d76
cyyeh <chihyu.jimmy.yeh@gmail.com>
2020-03-11 13:45:32 +0800

    update library

--- projects/youbike/src/visualization/visualize.py
+++ projects/youbike/src/visualization/visualize.py
@@ -2,31 +2,32 @@ import pandas as pd
 import matplotlib.pyplot as plt
 
 
-def draw_line_plot_by_column(df, font_prop=''):
+def draw_line_plot_by_column(df, font_prop='', color='white', label_size=14, title_size=16, fig_size=(12, 8)):
     for column in df.columns:
-        plt.figure(figsize=(12, 8))
+        plt.figure(figsize=fig_size)
         x = df.index
         y = df[column]
-        plt.xticks(color='white', fontsize=14)
-        plt.yticks(color='white', fontsize=14)
-        plt.title(column, fontproperties=font_prop, fontsize=16)
+        plt.xticks(color=color, fontsize=label_size)
+        plt.yticks(color=color, fontsize=label_size)
+        plt.title(column, fontproperties=font_prop,
+                  fontsize=title_size, color=color)
         plt.plot(x, y)
         plt.show()
         print()
 
 
-def draw_heatmap_by_column(df, font_prop=''):
-    f = plt.figure(figsize=(12, 12))
+def draw_heatmap_by_column(df, font_prop='', color='white', label_size=14, fig_size=(12, 12)):
+    f = plt.figure(figsize=fig_size)
     plt.matshow(df.corr(), fignum=f.number)
     plt.xticks(range(df.shape[1]), df.columns, fontproperties=font_prop,
-               fontsize=14, rotation=75, color='white')
+               fontsize=label_size, rotation=75, color=color)
     plt.yticks(range(df.shape[1]), df.columns,
-               fontproperties=font_prop, fontsize=14, color='white')
+               fontproperties=font_prop, fontsize=label_size, color=color)
     cb = plt.colorbar()
-    cb.ax.tick_params(labelsize=14, labelcolor='white', color='white')
+    cb.ax.tick_params(labelsize=label_size, labelcolor=color, color=color)
 
 
-def plot_youbike_mean_available_number(df, freq='realtime'):
+def plot_youbike_mean_available_number(df, freq='realtime', label_size=14, title_size=16, color='white', fig_size=(12, 8)):
     freq_dict = {
         'realtime': '5 Minutes(realtime)',
         'H': 'Hour',
@@ -41,12 +42,12 @@ def plot_youbike_mean_available_number(df, freq='realtime'):
 
     x = df_.index
     y = df_.current_number
-    plt.figure(figsize=(12, 8))
+    plt.figure(figsize=fig_size)
     plt.title(
         f"Youbike Mean Available Number per {freq_dict[freq]} at University of Taipei",
-        color='white',
-        fontsize=16
+        color=color,
+        fontsize=title_size
     )
-    plt.xticks(color='white', fontsize=14)
-    plt.yticks(color='white', fontsize=14)
+    plt.xticks(color=color, fontsize=label_size)
+    plt.yticks(color=color, fontsize=label_size)
     plt.plot(x, y)
